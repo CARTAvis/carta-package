a44a4efffd390528c2bd75b2267191c012931bfb
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2024-01-23 20:49:54 +0800

    add special handling for version flag

--- macOS_electron/carta-backend/bin/carta.sh
+++ macOS_electron/carta-backend/bin/carta.sh
@@ -6,6 +6,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/../etc"
 
 export CASAPATH="../../../../$DIR linux"
 
-$dirname/casa_data_autoupdate
+if [[ ! " $@ " =~ ( --version | -v | --help | -h ) ]]; then
+    if [ -x "$(command -v casa_data_autoupdate)" ]; then
+        $dirname/casa_data_autoupdate
+    fi
+fi
 
 $dirname/carta_backend $@ --frontend_folder=$dirname/../../
