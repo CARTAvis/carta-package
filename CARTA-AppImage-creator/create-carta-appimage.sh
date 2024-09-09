00c8d853029ddb1dfd3eafc970d4f38d97004d90
markccchiang <chengchin.chiang@gmail.com>
2024-09-09 14:28:40 +0800

    Rename the docker file and script to build the Linux AppImage

--- CARTA-AppImage-creator/create-carta-appimage.sh
+++ CARTA-AppImage-creator/create-carta-appimage.sh
@@ -58,7 +58,7 @@ else
   mkdir -p carta-frontend/build
 fi
 
-docker build -f Dockerfile-carta-appimage-create-Almalinux8 \
+docker build -f Dockerfile-carta-appimage-create \
              --build-arg ARCH_TYPE=$ARCH \
              --build-arg BASE_IMAGE=$IMAGE \
              --build-arg CASACORE=$CARTA_CASACORE_TAG \
