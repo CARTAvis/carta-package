370ba36763ffecd2a79348189796e0bc3ac0997d
markccchiang <chengchin.chiang@gmail.com>
2024-09-23 14:45:23 +0800

    Update the Mac electron builder for notarizing

--- macOS_electron/scripts/notarize.js
+++ macOS_electron/scripts/notarize.js
@@ -9,9 +9,9 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
-    appBundleId: '<Your Apple Developer ID> ',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: '<Your Apple Developer email address>',
-    appleIdPassword: '<Your app-specific-password>',
+    appleIdPassword: '<Your app specific password>',
+    teamId: '<Your Apple Team ID assigned by Apple>',
     });
   };
