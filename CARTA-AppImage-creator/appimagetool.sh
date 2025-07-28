0ac02d7e3cc5f05b65a417b5843d080889b57d9e
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2025-07-28 15:27:58 +0800

    fix download appimagetool

--- CARTA-AppImage-creator/appimagetool.sh
+++ CARTA-AppImage-creator/appimagetool.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+ARCH=$(arch)
+if [ ${ARCH} = "arm64" ]; then
+    ARCH="aarch64"
+fi
+
 # download the latest appimagetool
+echo "Downloading appimagetool for ${ARCH} architecture..."
 wget -c https://github.com/`wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-${ARCH}.AppImage" | head -n 1 | awk '{print $2}' | cut -d '"' -f 2`
-if [ -f appimagetool-${APPIMAGE_VERSION}-$(arch).AppImage ]; then
-    chmod 755 appimagetool-${APPIMAGE_VERSION}-$(arch).AppImage
-    APPIMAGE_EXTRACT_AND_RUN=1 ARCH=$(arch) VERSION=${VERSION} ./appimagetool-${APPIMAGE_VERSION}-$(arch).AppImage CARTA
+if [ -f appimagetool-${APPIMAGE_VERSION}-${ARCH}.AppImage ]; then
+    chmod 755 appimagetool-${APPIMAGE_VERSION}-${ARCH}.AppImage
+    APPIMAGE_EXTRACT_AND_RUN=1 ARCH=${ARCH} VERSION=${VERSION} ./appimagetool-${APPIMAGE_VERSION}-${ARCH}.AppImage CARTA
 fi
 
 # Extract a unique Embedded Signature
-if [ -x carta-${VERSION}-$(arch).AppImage ]; then
-    ./carta-${VERSION}-$(arch).AppImage --appimage-signature > Embedded-Signature
+if [ -x carta-${VERSION}-${ARCH}.AppImage ]; then
+    ./carta-${VERSION}-${ARCH}.AppImage --appimage-signature > Embedded-Signature
 fi
