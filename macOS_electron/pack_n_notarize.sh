d024a6eb33869fa95612c2c710c2016645abc8f9
Po-Sheng Huang <128131821+pshnghng0318@users.noreply.github.com>
2026-03-09 12:18:45 +0800

    Update notarization script for Electron packaging

--- macOS_electron/pack_n_notarize.sh
+++ macOS_electron/pack_n_notarize.sh
@@ -4,11 +4,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# Check if nvm has the node version
-if ! nvm ls "$NODE_VERSION" > /dev/null 2>&1; then
-    nvm install "$NODE_VERSION"
-fi
-nvm use "$NODE_VERSION"
+nvm use 22
 
 source ./dmg_config
 
@@ -17,10 +13,10 @@ cd ${PACKAGING_PATH}/pack
 rm -rf ${PACKAGING_PATH}/pack/dist
 
 # notarize setting #
-password=$(/opt/homebrew/bin/openssl enc -aes-256-cbc -d -a -salt -iter 100 -pass pass:Qi0GBwUgrLFC1gAcp6di -in /Users/acdc/encrypted_password.enc)
+password=$(/opt/homebrew/bin/openssl enc -aes-256-cbc -d -a -salt -iter 100 -pass pass:YOUR_PASS -in /Users/acdc/encrypted_password.enc)
 security unlock-keychain -p $password ~/Library/Keychains/login.keychain-db
 
-export DEBUG=electron-notarize*
+export DEBUG=@electron/notarize*
 export APPLE_APP_SPECIFIC_PASSWORD=
 export APPLE_ID=
 export APPLE_TEAM_ID=
@@ -31,8 +27,8 @@ export API_KEY_ISSUER_ID=
 npm install
 npm install -g electron-builder
 
-rm -rf node_modules/deasync/bin/darwin-x64-node-0.10
-rm -rf node_modules/deasync/bin/darwin-x64-node-0.11
-rm -rf node_modules/deasync/bin/darwin-x64-node-0.12
-
-${PACKAGING_PATH}/pack/node_modules/.bin/electron-builder build --mac --${ARCH}
+if [ "$ARCH" == "arm64" ]; then
+    ${PACKAGING_PATH}/pack/node_modules/.bin/electron-builder build --mac --${ARCH}
+else
+    ${PACKAGING_PATH}/pack/node_modules/.bin/electron-builder build --mac --x64
+fi
