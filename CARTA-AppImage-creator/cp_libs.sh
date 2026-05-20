2be4c8ef477d1358d6544650ab368e471b151201
Po-Sheng Huang <128131821+pshnghng0318@users.noreply.github.com>
2026-05-20 16:58:00 +0800

    Update library versions and add new dependencies
    Updated library versions for casa and added new dependencies.

--- CARTA-AppImage-creator/cp_libs.sh
+++ CARTA-AppImage-creator/cp_libs.sh
@@ -8,21 +8,21 @@ patchelf --set-rpath "\$ORIGIN/lib" ${DOCKER_PACKAGING_PATH}/CARTA/bin/carta_bac
 mkdir -p ${DOCKER_PACKAGING_PATH}/CARTA/lib 
 cp /usr/lib64/libaec.so.0 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
 cp /usr/lib64/libblas.so.3 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_casa.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_coordinates.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_derivedmscal.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_fits.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_imageanalysis.so.6 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_images.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_lattices.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_meas.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_measures.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_mirlib.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_msfits.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_ms.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_scimath.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_scimath_f.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
-cp /opt/carta-casacore/lib/libcasa_tables.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_casa.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_coordinates.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_derivedmscal.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_fits.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_imageanalysis.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_images.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_lattices.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_meas.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_measures.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_mirlib.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_msfits.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_ms.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_scimath.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_scimath_f.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /opt/carta-casacore/lib/libcasa_tables.so.9 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
 cp /opt/cfitsio/lib/libcfitsio.so.10 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
 cp /usr/lib64/libgfortran.so.5 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
 cp /usr/lib64/libgomp.so.1 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
@@ -45,6 +45,13 @@ cp /usr/lib64/libtinfo.so.6 ${DOCKER_PACKAGING_PATH}/CARTA/lib
 cp /usr/lib64/libwcs.so.7 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
 cp /usr/lib64/libzfp.so.0 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
 cp /usr/lib64/libzstd.so.1 ${DOCKER_PACKAGING_PATH}/CARTA/lib 
+cp /usr/lib64/libre2.so.0 ${DOCKER_PACKAGING_PATH}/CARTA/lib
+cp /usr/lib64/libsnappy.so.1 ${DOCKER_PACKAGING_PATH}/CARTA/lib
+cp /usr/lib64/libuv.so.1 ${DOCKER_PACKAGING_PATH}/CARTA/lib
+cp /usr/lib64/liblz4.so.1 ${DOCKER_PACKAGING_PATH}/CARTA/lib
+cp /usr/lib64/liblzma.so.5 ${DOCKER_PACKAGING_PATH}/CARTA/lib
+cp /usr/lib64/libbz2.so.1 ${DOCKER_PACKAGING_PATH}/CARTA/lib
+cp /usr/lib64/libblosc.so.1 ${DOCKER_PACKAGING_PATH}/CARTA/lib
 if [ $(arch) = "x86_64" ]; then cp /usr/lib64/libquadmath.so.0 ${DOCKER_PACKAGING_PATH}/CARTA/lib; fi
 
 # copy some additional files
@@ -58,4 +65,3 @@ mkdir -p ${DOCKER_PACKAGING_PATH}/CARTA/usr/share/icons/hicolor/128x128/apps
 cp -r ${DOCKER_PACKAGING_PATH}/carta-backend/static/icons/256x256/cartaviewer.png ${DOCKER_PACKAGING_PATH}/CARTA/usr/share/icons/hicolor/256x256/apps/
 cp -r ${DOCKER_PACKAGING_PATH}/carta-backend/static/icons/128x128/cartaviewer.png ${DOCKER_PACKAGING_PATH}/CARTA/usr/share/icons/hicolor/128x128/apps/
 mkdir -p ${DOCKER_PACKAGING_PATH}/CARTA/usr/share/metainfo
-
