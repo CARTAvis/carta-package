d1b3b674a9a56083fa872f38ba0c8c9fd92e28e4
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2026-05-26 16:57:44 +0800

    Revert non-main.js/package.json changes to match master

--- macOS_electron/pack_n_notarize.sh
+++ macOS_electron/pack_n_notarize.sh
@@ -12,25 +12,18 @@ cd ${PACKAGING_PATH}/pack
 rm -rf ${PACKAGING_PATH}/pack/dist
 
 # notarize setting #
-password=$(/usr/local/opt/openssl@3/bin/openssl enc -aes-256-cbc -d -a -salt -iter 100 -pass pass:PASS_KEY -in /Users/acdc/encrypted_password.enc)
-export CSC_LINK=
-export CSC_KEY_PASSWORD=
+password=$(/opt/homebrew/bin/openssl enc -aes-256-cbc -d -a -salt -iter 100 -pass pass:YOUR_PASS -in /WHERE_IS_YOUR_encrypted_password.enc)
+security unlock-keychain -p $password ~/Library/Keychains/login.keychain-db
 
 export DEBUG=@electron/notarize*
 export APPLE_APP_SPECIFIC_PASSWORD=
-export APPLE_ID_PASSWORD=
 export APPLE_ID=
 export APPLE_TEAM_ID=
-export CSC_PARALLELIZATION=false
+export API_KEY_ID=
+export API_KEY_ISSUER_ID=
 # notarize setting #
 
 npm install
 npm install -g electron-builder
 
-
-if [ $ARCH == "arm64" ]; then
-    ${PACKAGING_PATH}/pack/node_modules/.bin/electron-builder build --mac --arm64
-else
-    ${PACKAGING_PATH}/pack/node_modules/.bin/electron-builder build --mac --x64
-fi
-
+${PACKAGING_PATH}/pack/node_modules/.bin/electron-builder build --mac --${ARCH}
