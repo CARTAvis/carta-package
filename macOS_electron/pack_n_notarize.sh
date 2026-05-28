4164a28504ff9c2cf7b779b93e3a53e08de96bb5
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2026-05-28 13:30:52 +0800

    macOS electron: FITS file association

--- macOS_electron/pack_n_notarize.sh
+++ macOS_electron/pack_n_notarize.sh
@@ -12,18 +12,25 @@ cd ${PACKAGING_PATH}/pack
 rm -rf ${PACKAGING_PATH}/pack/dist
 
 # notarize setting #
-password=$(/opt/homebrew/bin/openssl enc -aes-256-cbc -d -a -salt -iter 100 -pass pass:YOUR_PASS -in /WHERE_IS_YOUR_encrypted_password.enc)
-security unlock-keychain -p $password ~/Library/Keychains/login.keychain-db
+password=$(/usr/local/opt/openssl@3/bin/openssl enc -aes-256-cbc -d -a -salt -iter 100 -pass pass:PASS_KEY -in /Users/acdc/encrypted_password.enc)
+export CSC_LINK=
+export CSC_KEY_PASSWORD=
 
 export DEBUG=@electron/notarize*
 export APPLE_APP_SPECIFIC_PASSWORD=
+export APPLE_ID_PASSWORD=
 export APPLE_ID=
 export APPLE_TEAM_ID=
-export API_KEY_ID=
-export API_KEY_ISSUER_ID=
+export CSC_PARALLELIZATION=false
 # notarize setting #
 
 npm install
 npm install -g electron-builder
 
-${PACKAGING_PATH}/pack/node_modules/.bin/electron-builder build --mac --${ARCH}
+
+if [ $ARCH == "arm64" ]; then
+    ${PACKAGING_PATH}/pack/node_modules/.bin/electron-builder build --mac --arm64
+else
+    ${PACKAGING_PATH}/pack/node_modules/.bin/electron-builder build --mac --x64
+fi
+
